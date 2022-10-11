ed9f9e38ca29f98defd7eac37ab2a5cbeef9fe06
wlstjd2116 <102150793+wlstjd2116@users.noreply.github.com>
2022-10-11 17:50:32 +0900

    saved personal pj

--- db/BookList.js
+++
@@ -1,5 +0,0 @@
-var bookList = new Array();
-
-bookList = [
-    
-];
---
+++ db/MemberList.js
@@ -0,0 +1,3 @@
+var memberList = { wlstjd2116: "1234" };
+// var wlstjd2116 = [~]
+// var mbHtList = {wlstjd2116 : [, , , , , .... ], lis981026 : [, , ,, , , ,,... ]}
